a539d945e965f06a5b843ca1697a315f9c8eb1d9
Ebuka Harry <ebukaharry6@gmail.com>
2024-08-15 06:00:01 +0100

    Mobile navigation

---
+++ src/components/Header.vue
@@ -0,0 +1,46 @@
+<template>
+  <nav class="p-4 md:bg-transparent bg-white md:flex">
+    <div class="flex justify-between items-center">
+      <a href="#" class="text-gray-800 text-2xl font-bold w-[100px]"><img src="../assets/images/logo.png" alt=""></a>
+      <button class="text-gray-800 md:hidden" @click="toggleMenu">menu</button>
+    </div>
+    <div :class="['mt-16 md:mt-0 flex flex-col h-[80vh] md:h-auto md:flex-row md:justify-between w-full items-center', { block: isMenuOpen, hidden: !isMenuOpen }]">
+      <ul class="md:flex space-y-4 md:space-y-0 md:space-x-6 p-4">
+        <p class="text-gray-400 md:hidden">Menu</p>
+        <li v-for="link in navLinks" :key="link.text" class="text-gray-800 ">
+          <a :href="link.href" class=" hover:underline hover:underline-offset-4 hover:decoration-2 font-400 text-4xl">{{ link.text }}</a>
+        </li>
+
+        <div class="get-in-touch pt-32 md:hidden">
+            <p class="text-gray-400 mb-2">Get in touch</p>
+            <a class="underline underline-offset-4 decoration-gray-300" href="mailto:info@nikaosolutions.com">info@nikaosolutions.com</a>
+        </div>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isMenuOpen: false,
+      navLinks: [
+        { text: 'What we do', href: '#' },
+        { text: 'Projects', href: '#' },
+        { text: 'Company', href: '#' },
+        { text: 'Contacts', href: '#' }
+      ]
+    }
+  },
+  methods: {
+    toggleMenu() {
+      this.isMenuOpen = !this.isMenuOpen
+    }
+  }
+}
+</script>
+
+<style scoped>
+/* Additional custom styles can be added here if needed */
+</style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import './assets/main.css'
+import './css/tailwind.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
